ad67cdf8f107018c83777848a5654ed5e103e5bc
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2023-12-04 10:36:55 +0100

    initialize branch 17.0

--- script/extract_addons.py
+++ script/extract_addons.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 # Quick and dirty script to manually get some addons all OCA addons
 # To remove once module analysis install on all our instances
 from pathlib import Path
@@ -6,86 +7,89 @@ import tempfile
 import shutil
 import argparse
 
-ODOO_SERIE = "16.0"
+ODOO_SERIE = "17.0"
 ADDONS_TO_EXTRACT = {
-    "account-closing": (
-        "account_cutoff_picking",
-        "account_cutoff_base",
-        "account_invoice_start_end_dates",
-        "account_cutoff_start_end_dates",
+    # "account-closing": (
+    #     "account_cutoff_picking",
+    #     "account_cutoff_base",
+    #     "account_invoice_start_end_dates",
+    #     "account_cutoff_start_end_dates",
+    # ),
+    # "account-financial-reporting": (
+    #     "account_financial_report",
+    # ),
+    # "account-invoice-reporting": (
+    #     "account_invoice_report_grouped_by_picking",
+    # ),
+    # "bank-statement-import": (
+    #     "account_statement_import_base",
+    #     "account_statement_import_camt",
+    #     "account_statement_import_camt54",
+    #     "account_statement_import_file",
+    #     "account_statement_import_ofx",
+    # ),
+    # "reporting-engine": (
+    #     "bi_sql_editor",
+    #     "report_wkhtmltopdf_param",
+    #     "report_xlsx",
+    #     "sql_request_abstract",
+    # ),
+    # "connector": (
+    #     "connector",
+    #     "component",
+    #     "component_event",
+    # ),
+    # # repo name, org, branch
+    # # TODO: pending PR https://github.com/OCA/connector-interfaces/pull/129
+    # ("connector-interfaces", "camptocamp", "16-connector_importer_product"): (
+    #     "connector_importer",
+    #     "connector_importer_product",
+    # ),
+    "partner-contact": (
+        "partner_address_street3",
     ),
-    "account-financial-reporting": (
-        "account_financial_report",
-    ),
-    "account-invoice-reporting": (
-        "account_invoice_report_grouped_by_picking",
-    ),
-    "bank-statement-import": (
-        "account_statement_import_base",
-        "account_statement_import_camt",
-        "account_statement_import_camt54",
-        "account_statement_import_file",
-        "account_statement_import_ofx",
-    ),
-    "reporting-engine": (
-        "bi_sql_editor",
-        "report_wkhtmltopdf_param",
-        "report_xlsx",
-        "sql_request_abstract",
-    ),
-    "connector": (
-        "connector",
-        "component",
-        "component_event",
-    ),
-    # repo name, org, branch
-    # TODO: pending PR https://github.com/OCA/connector-interfaces/pull/129
-    ("connector-interfaces", "camptocamp", "16-connector_importer_product"): (
-        "connector_importer",
-        "connector_importer_product",
-    ),
-    "pos": (
-        "pos_lot_barcode",
-        "pos_lot_selection",
-    ),
-    "purchase-workflow": (
-        "purchase_tier_validation",
-        "purchase_requisition_tier_validation",
-        "purchase_advance_payment",
-    ),
-    "server-ux": (
-        "date_range",
-        "server_action_mass_edit",
-        "base_tier_validation",
-        "base_tier_validation_formula",
-    ),
-    "stock-logistics-reporting":(
-        "stock_picking_report_valued"
-    ),
-    "stock-logistics-workflow": (
-        "stock_picking_invoice_link",
-    ),
-    "mis-builder": (
-        "mis_builder",
-        "mis_builder_budget",
-        "mis_builder_demo",
-    ),
-    ("mis-builder-contrib", "dzungtran89", "16.0-mis_builder_budget_product"): (
-        "mis_builder_budget_product",
-    ),
-    "mis-builder-contrib": (
-        "mis_builder_total_committed_purchase",
-    ),
-    "queue": (
-        "queue_job",
-        "queue_job_cron_jobrunner",
-    ),
-    ("smartcamp_core_private", "camptocamp", "16.0"): (
-        "payment_provider_wallee",
-    ),
-    "web": (
-        "web_advanced_search",
-    ),
+    # "pos": (
+    #     "pos_lot_barcode",
+    #     "pos_lot_selection",
+    # ),
+    # "purchase-workflow": (
+    #     "purchase_tier_validation",
+    #     "purchase_requisition_tier_validation",
+    #     "purchase_advance_payment",
+    # ),
+    # "server-ux": (
+    #     "date_range",
+    #     "server_action_mass_edit",
+    #     "base_tier_validation",
+    #     "base_tier_validation_formula",
+    # ),
+    # "stock-logistics-reporting":(
+    #     "stock_picking_report_valued"
+    # ),
+    # "stock-logistics-workflow": (
+    #     "stock_picking_invoice_link",
+    # ),
+    # "mis-builder": (
+    #     "mis_builder",
+    #     "mis_builder_budget",
+    #     "mis_builder_demo",
+    # ),
+    # ("mis-builder-contrib", "dzungtran89", "16.0-mis_builder_budget_product"): (
+    #     "mis_builder_budget_product",
+    # ),
+    # "mis-builder-contrib": (
+    #     "mis_builder_total_committed_purchase",
+    # ),
+    # "queue": (
+    #     "queue_job",
+    #     "queue_job_cron_jobrunner",
+    # ),
+    # ("smartcamp_core_private", "camptocamp", "16.0"): (
+    #     "payment_provider_wallee",
+    # ),
+    # "web": (
+    #     "web_advanced_search",
+    # ),
 }
 
 
